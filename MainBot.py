931344feac4c229dd1dc63f8c97af6b2984a2076
SIAILYA <sia.ilya.2017@gmail.com>
2019-10-04 19:51:05 +0300

    1.0.2B - Небольшие поправки в коде.

--- MainBot.py
+++ MainBot.py
@@ -14,10 +14,11 @@ class Bot:
         self.vk_api = self.vk.get_api()
 
     def photo(self, send_id, root='img.jpg'):
-        request = requests.post(self.vk.method('photos.getMessagesUploadServer')[
-                                    'upload_url'], files={'photo': open(root, 'rb')}).json()
-        save_photo = self.vk_api.photos.saveMessagesPhoto(
-            photo=request['photo'], server=request['server'], hash=request['hash'])[0]
+        request = requests.post(self.vk_api.photos.getMessagesUploadServer()['upload_url'],
+                                files={'photo': open(root, 'rb')}).json()
+        save_photo = \
+        self.vk_api.photos.saveMessagesPhoto(photo=request['photo'], server=request['server'],
+                                             hash=request['hash'])[0]
         photo = f'photo{save_photo["owner_id"]}_{save_photo["id"]}'
         self.vk_api.messages.send(peer_id=send_id, message='',
                                   random_id=randint(-1000, 1000), attachment=photo)
@@ -34,14 +35,18 @@ class Bot:
     def inbox(self, event):
         msg = event.obj.text.replace(' ', '').replace('"', '').upper()
         date = get_tomorrow_date()
-        if (msg[-1] in 'АБВГабвг') and (msg[:-1] in '567891011'):
+        if msg in ['5А', '5Б', '5В', '5Г', '6А', '6Б', '6В', '7А', '7Б', '7В', '8А', '8Б', '8В',
+                   '9А', '9Б', '9В', '10А', '810Б', '10В', '10Г', '11А', '11Б', '11В', '11Г']:
             if path.exists(f'{date}/{msg}.jpg'):
                 self.photo(event.obj.peer_id, f'{date}/{msg.upper()}.jpg')
             else:
                 self.send_msg(event.obj.peer_id, 'Сейчас все будет. Скачиваю и режу расписание')
                 SF()
                 self.photo(event.obj.peer_id, f'{date}/{msg.upper()}.jpg')
-
+        else:
+            self.send_msg(event.obj.peer_id, 'Такого класса не существует!\nПри вводе класса '
+                                             'используйте только цифры и кириллицу!\nНапример - '
+                                             '11А, 9"В", 10 Г')
 
 if __name__ == "__main__":
     Bot().main()
