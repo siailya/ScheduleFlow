c2a789181074f33f0363939d48c5197f7e490fb0
SIAILYA <sia.ilya.2017@gmail.com>
2019-10-06 22:19:39 +0300

    1.1.2B - Улучшена работа выдачи расписания по дате.

--- Process.py
+++ Process.py
@@ -9,19 +9,29 @@ from PIL import Image, ImageFilter
 
 def get_date(date=''):
     if not date:
-        if pendulum.tomorrow().date().weekday() != 6:
-            return str(pendulum.tomorrow().date().__format__('DD.MM.YYYY'))
-        else:
-            tm = pendulum.tomorrow().day + 1
-            mt = pendulum.tomorrow().month
-            if (tm <= 31) and (mt in [1, 3, 5, 7, 8, 10, 12]):
-                return str(pendulum.date(pendulum.tomorrow().year, pendulum.tomorrow().month, tm))
-            elif (tm <= 30) and (mt in [2, 4, 6, 9, 11]):
-                return str(pendulum.date(pendulum.tomorrow().year, pendulum.tomorrow().month, tm))
+        if pendulum.now(tz='Europe/Moscow').time().hour >= 16 and pendulum.now(
+                tz='Europe/Moscow').time().hour < 0:
+            if pendulum.tomorrow().date().weekday() != 6:
+                return str(pendulum.tomorrow().date().__format__('DD.MM.YYYY'))
             else:
-                tm = 1
-                mt += 1
-                return str(pendulum.date(pendulum.tomorrow().year, mt, tm))
+                tm = pendulum.tomorrow().day + 1
+                mt = pendulum.tomorrow().month
+                if (tm <= 31) and (mt in [1, 3, 5, 7, 8, 10, 12]):
+                    return str(pendulum.date(pendulum.tomorrow().year, pendulum.tomorrow().month,
+                                             tm).__format__('DD.MM.YYYY'))
+                elif (tm <= 30) and (mt in [2, 4, 6, 9, 11]):
+                    return str(pendulum.date(pendulum.tomorrow().year, pendulum.tomorrow().month,
+                                             tm).__format__('DD.MM.YYYY'))
+                else:
+                    tm = 1
+                    mt += 1
+                    return str(pendulum.date(pendulum.tomorrow().year, mt, tm).__format__(
+                        'DD.MM.YYYY'))
+        else:
+            if pendulum.today().date().weekday() != 6:
+                return str(pendulum.today().date().__format__('DD.MM.YYYY'))
+            else:
+                return str(pendulum.tomorrow().date().__format__('DD.MM.YYYY'))
     else:
         return date
 
