07c8114adc163e6cd19348ccda8356869dbed7bf
SIAILYA <sia.ilya.2017@gmail.com>
2020-01-09 08:33:10 +0300

    Забыл логгер

--- bot/stuff/Config.py
+++ bot/stuff/Config.py
@@ -13,20 +13,20 @@ class Config:
     else:
         PATH = '/root/ScheduleFlow/'
     UPDATE_INTERVAL = 15
-    SCHEDULE_LINK = 'https://амтэк35.рф/wp-content/uploads/'
+    SCHEDULE_LINK = 'https://амтэк35.рф/wp-content/uploads/'  # date.png
     PARSE_INTERVAL = 30
     REDIRECT_DATE = '13.01.2020'
 
     ADMINS = [223632391, 222383631, 66061219]
     if TESTING:
-        TOKEN = 'T-O-K-E-N'
+        TOKEN = '46f3beec75a013ae0556c7558cf031eb56912a7ae17c2e6bd4c8c9c999006a953a9661ca31f3d28ac5dbe'
         CONSOLE = 2000000001
         GROUP_ID = 187427285
         PREFIX = '[club187427285|sftest] '
     else:
-        TOKEN = 'T-O-K-E-N'
+        TOKEN = 'f3d3d7283516f74feee0b753c2e0063e53ffe0bde3f9c99efd0a1ea039310eca2f6c697be8ee7c857ab06'
         CONSOLE = 2000000002
         GROUP_ID = 187161295
         PREFIX = '[club187161295|scheduleflow] '
 
-    DIALOG_FLOW_TOKEN = 'T-O-K-E-N'
+    DIALOG_FLOW_TOKEN = 'daad299f70fe4168b16332551d267f9a'
